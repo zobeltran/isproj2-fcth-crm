f19ca65dfc2deac31bad4ae3efb484f10c932509
Renzo Beltran <renzoe.beltran@gmail.com>
2017-10-06 13:15:09 +0800

    Added promo blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@
 The init file of the main application
 """
 from flask import Flask
+from app.promo.routes import MOD_PROMO
 
 APP = Flask(__name__, static_folder=None)
+
+APP.register_blueprint(MOD_PROMO)
 
 if __name__ == '__main__':
     APP.run()
