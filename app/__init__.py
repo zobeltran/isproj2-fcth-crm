6ec1b1b8fddad98e7d86412615128e42ac783dc5
zobeltran <renzoe.beltran@gmail.com>
2017-10-11 19:33:44 +0800

    Modified init file

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,18 @@
 The init file of the main application
 """
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from app.promo.routes import MOD_PROMO
 
+# Flask Initialization
 APP = Flask(__name__, static_folder=None)
-
+# Database Initialization
+DB = SQLAlchemy(APP)
 APP.register_blueprint(MOD_PROMO)
 
+
+APP.config.from_object('config.DevelopmentConfig')
+
 if __name__ == '__main__':
+    APP.jinja_env.cache = {}
     APP.run()
