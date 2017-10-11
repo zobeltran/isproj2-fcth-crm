4a9d7c4bdc9c63a44909643e098407709724330c
zobeltran <renzoe.beltran@gmail.com>
2017-10-11 19:13:46 +0800

    Seperated class on DevConfig and ProdConfig

--- config.py
+++ config.py
@@ -1,4 +1,28 @@
 """
-This is the configuration file
+This is the Configuration
 """
-SECRET_KEY = '\x0e\xe4~C"^\xe5\xfb\x1e\x12/\x8d\x8f\x1f7\x13V,\xd5 <F\xde\xca'
+class BaseConfig(object):
+    """
+    This is the main config class to be inherited
+    """
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(BaseConfig):
+    """
+    This is the main configuration class
+    """
+    DEBUG = True
+    DATABASE_URI = 'mysql://devusertesting:devuser@localhost/crmfcth'
+    SECRET_KEY = 'developmentsecretkey'
+    UPLOAD_FOLDER = 'app/upload'
+class ProductionConfig(BaseConfig):
+    """
+    This is the production configuration class
+    """
+    DATABASE = ''.join(['postgres://zccisfiglvdagd:',
+                        '5d5fe904f02e350ca33066c2a51a5c6cc0c5236ade95b403cea2418d0ca567c2',
+                        '@ec2-54-243-255-57.compute-1.amazonaws.com:5432/dfn8ta0mogh2u9'])
+    SECRET_KEY = '\xd6;\x9aq)\xea\xa5\x9c\xf5BM^N\x05X\\\xec5w\x0f\xb9\xab\xf9\xb8'
+    UPLOAD_FOLDER = 'app/upload'
+    DATABASE_URI = DATABASE
