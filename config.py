d8b753c4429ad53af42e868991b4c812708cb656
zobeltran <renzoe.beltran@gmail.com>
2017-10-17 15:57:22 +0800

    Added postgresql on devConfig

--- config.py
+++ config.py
@@ -13,16 +13,17 @@ class DevelopmentConfig(BaseConfig):
     This is the main configuration class
     """
     DEBUG = True
-    DATABASE_URI = 'mysql://devusertesting:devuser@localhost/crmfcth'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:admin@localhost/crmfcth'
     SECRET_KEY = 'developmentsecretkey'
     UPLOAD_FOLDER = 'app/upload'
+
 class ProductionConfig(BaseConfig):
     """
     This is the production configuration class
     """
-    DATABASE = ''.join(['postgres://zccisfiglvdagd:',
+    DATABASE = ''.join(['postgresql://zccisfiglvdagd:',
                         '5d5fe904f02e350ca33066c2a51a5c6cc0c5236ade95b403cea2418d0ca567c2',
                         '@ec2-54-243-255-57.compute-1.amazonaws.com:5432/dfn8ta0mogh2u9'])
     SECRET_KEY = '\xd6;\x9aq)\xea\xa5\x9c\xf5BM^N\x05X\\\xec5w\x0f\xb9\xab\xf9\xb8'
     UPLOAD_FOLDER = 'app/upload'
-    DATABASE_URI = DATABASE
+    SQLALCHEMY_DATABASE_URI = DATABASE
