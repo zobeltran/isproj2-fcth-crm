035c27831833abe03fbc4532402b5b65584a6b6c
zobeltran <renzoe.beltran@gmail.com>
2017-10-18 10:41:19 +0800

    Fixed models for Hotel and Flight Request

--- app/promo/models.py
+++ app/promo/models.py
@@ -26,12 +26,12 @@ class HotelBookings(DB.Model):
     Hotel Bookings Model
     """
     hbooking_id = DB.Column(DB.Integer, primary_key=True)
-    hbooking_name = DB.Column(DB.String(100), nullable=False)
-    hbooking_description = DB.Column(DB.String(200), nullable=False)
-    hbooking_price = DB.Column(DB.Float)
-    hbooking_createtime = DB.Column(DB.DateTime)
-    hbooking_updated = DB.Column(DB.DateTime)
-    __tablename__ = 'Hotel_Booking'
+    hbooking_location = DB.Column(DB.String(200), nullable=False)
+    hbooking_check_in = DB.Column(DB.String(50))
+    hbooking_check_out = DB.Column(DB.String(50))
+    hbooking_number_of_rooms = DB.Column(DB.Integer)
+    hbooking_budget_range = DB.Column(DB.Float)
+    __tablename__ = 'Hotel_Booking_Request'
 
     def __repr__(self):
         return '<Hotel %r)' % self.hbooking_name
@@ -41,12 +41,13 @@ class FlightBooking(DB.Model):
     Flight Bookings Model
     """
     fbooking_id = DB.Column(DB.Integer, primary_key=True)
-    fbooking_name = DB.Column(DB.String(100), nullable=False)
-    fbooking_description = DB.Column(DB.String(200), nullable=False)
-    fbooking_price = DB.Column(DB.Float)
-    fbooking_createtime = DB.Column(DB.DateTime)
-    fbooking_updated = DB.Column(DB.DateTime)
-    fbooking__tablename__ = 'Flight_Booking'
+    fbooking_departure = DB.Column(DB.String(100), nullable=False)
+    fbooking_arrival = DB.Column(DB.String(200), nullable=False)
+    fbooking_departure_date = DB.Column(DB.String(50), nullable=False)
+    fbooking_arrival_date = DB.Column(DB.String(50), nullable=False)
+    fbooking_budget_range = DB.Column(DB.Float)
+    fbooking_head_count = DB.Column(DB.Integer)
+    fbooking__tablename__ = 'Flight_Booking_Request'
 
     def __repr__(self):
-        return '<Flight %r)' % self.fbooking_name
+        return '<Flight %r - %r)' % self.fbooking_departure, self.fbooking_arrival
