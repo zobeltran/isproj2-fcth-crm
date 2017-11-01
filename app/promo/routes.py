34453ab9da6212bca178de75d9b4e9c7750e872b
zobeltran <renzoe.beltran@gmail.com>
2017-11-01 19:52:16 +0800

    Fixed def bookings causing compilation errors

--- app/promo/routes.py
+++ app/promo/routes.py
@@ -5,7 +5,8 @@ from flask import Blueprint, render_template
 from app.promo.models import Packages, HotelBookings, FlightBooking
 from app.promo import forms
 MOD_PROMO = Blueprint('promo', __name__, template_folder='templates',
-                      static_folder='static', static_url_path='/%s' %__name__)
+                      static_folder='static', static_url_path='/%s' % __name__)
+
 
 @MOD_PROMO.route('/')
 def promos():
@@ -13,11 +14,13 @@ def promos():
     packages = Packages.query.order_by(Packages.package_id.desc()).all()
     return render_template("packages.html", packages=packages)
 
+
 @MOD_PROMO.route('/package/<int:package_id>')
 def package():
     """ Package """
     packages = Packages.query.all()
     return render_template('package.html', packages=packages)
+
 
 @MOD_PROMO.route('/flights', methods=['GET', 'POST'])
 def flights():
@@ -28,14 +31,15 @@ def flights():
         return '<h1>Flight Destination: ' + form.place_to.data + '</h1> Validate on Submit'
     print(form.errors)
     # if form.is_submitted():
-        # return '<h1>Flight Destination:' + form.place_to.data + '</h1> Submitted'
+    # return '<h1>Flight Destination:' + form.place_to.data + '</h1> Submitted'
     return render_template("flightbooking.html", form=form)
 
+
 @MOD_PROMO.route('/bookings', methods=['GET', 'POST'])
-def bookings
-  """ Bookings """
-  form = forms.HotelForm()
-  if form.validate_on_submit():
-    return '<h1>Hotel Bookings: ' + form.hotel_location.data '</h1>'
-  print(form.errors)
-  return render_template("hotelbooking.html", form=form)
+def bookings():
+    """ Bookings """
+    form = forms.HotelForm()
+    if form.validate_on_submit():
+        return '<h1>Hotel Bookings: ' + form.hotel_location.data + '</h1>'
+    print(form.errors)
+    return render_template("hotelbooking.html", form=form)
