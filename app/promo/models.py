7b3e82c185e0301bc5f851d473fb8c69e27529c0
zobeltran <renzoe.beltran@gmail.com>
2017-11-05 15:20:19 +0800

    Fixed model strings

--- app/promo/models.py
+++ app/promo/models.py
@@ -31,6 +31,7 @@ class HotelBookings(DB.Model):
     Hotel Bookings Model
     """
     hbooking_id = DB.Column(DB.Integer, primary_key=True)
+    hbooking_email = DB.Column(DB.String(100))
     hbooking_location = DB.Column(DB.String(200), nullable=False)
     hbooking_check_in = DB.Column(DB.String(50))
     hbooking_check_out = DB.Column(DB.String(50))
@@ -47,10 +48,11 @@ class FlightBooking(DB.Model):
     Flight Bookings Model
     """
     fbooking_id = DB.Column(DB.Integer, primary_key=True)
+    fbooking_email = DB.Column(DB.String(100))
     fbooking_departure = DB.Column(DB.String(100), nullable=False)
     fbooking_arrival = DB.Column(DB.String(200), nullable=False)
-    fbooking_departure_date = DB.Column(DB.String(50), nullable=False)
-    fbooking_arrival_date = DB.Column(DB.String(50), nullable=False)
+    fbooking_departure_date = DB.Column(DB.Date, nullable=False)
+    fbooking_arrival_date = DB.Column(DB.Date, nullable=False)
     fbooking_budget_range = DB.Column(DB.Float)
     fbooking_head_count = DB.Column(DB.Integer)
     __tablename__ = 'Flight_Booking_Request'
